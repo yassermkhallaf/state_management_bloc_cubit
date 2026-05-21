d815b2d713256cce4e452d5f3224f9ae061270a0
yasserkhallaf <yasser.m.khallaf2017@gmail.com>
2026-05-21 09:57:59 +0300

    Make todo app

--- lib/controllers/cubit/counter_cubit.dart
+++ lib/controllers/cubit/counter_cubit.dart
@@ -1,8 +1,27 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
-class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+class CounterCubit extends HydratedCubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() {
+    emit(state + 1);
+  }
+
+  void decrement() {
+    emit(state - 1);
+  }
+
+  @override
+  int? fromJson(Map<String, dynamic> json) {
+    return json["count"];
+  }
+
+  @override
+  Map<String, dynamic>? toJson(int state) {
+    return {"count": state};
+  }
 }

b8c32059b4558318c6103182f270ab42f04f9dc5
yasserkhallaf <yasser.m.khallaf2017@gmail.com>
2026-05-21 10:39:42 +0300

    Add blocListener

--- lib/main.dart
+++ lib/main.dart
@@ -37,42 +37,53 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => CounterCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        body: Center(
-          child: BlocBuilder<CounterCubit, int>(
+      child: BlocListener<CounterCubit, int>(
+        listener: (context, state) {
+          if (state == 5) {
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Counter = 5")));
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+            backgroundColor: Theme
+                .of(context)
+                .colorScheme
+                .inversePrimary,
+          ),
+          body: Center(
+            child: BlocBuilder<CounterCubit, int>(
+              builder: (context, state) {
+                return Text(
+                  state.toString(),
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25),
+                );
+              },
+            ),
+          ),
+          floatingActionButton: BlocBuilder<CounterCubit, int>(
             builder: (context, state) {
-              return Text(
-                state.toString(),
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25),
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterCubit>().increment();
+                    },
+                    child: Icon(Icons.add),
+                  ),
+                  const SizedBox(height: 10),
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterCubit>().decrement();
+                    },
+                    child: Icon(Icons.minimize),
+                  ),
+                ],
               );
             },
           ),
-        ),
-        floatingActionButton: BlocBuilder<CounterCubit, int>(
-          builder: (context, state) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    context.read<CounterCubit>().increment();
-                  },
-                  child: Icon(Icons.add),
-                ),
-                const SizedBox(height: 10),
-                FloatingActionButton(
-                  onPressed: () {
-                    context.read<CounterCubit>().decrement();
-                  },
-                  child: Icon(Icons.minimize),
-                ),
-              ],
-            );
-          },
         ),
       ),
     );
